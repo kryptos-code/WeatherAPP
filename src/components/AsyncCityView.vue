856c4422474634548d0591da972614911fbe0724
Shoaib Rehman <shoaib.rehman@wipro.com>
2024-07-02 19:52:25 +0530

    Initial Commit

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const weatherApiKey = "6909abb863a9067ca0545a83bbb20953";
+const route = useRoute();
+const getWeatherData = async () => {
+    try {
+        const weatherData = await axios.get(`https://api.weatherstack.com/current?access_key=${weatherApiKey}&query=${route.query.lat},${route.query.lng}}`);
+    } catch (error) {
+        console.log(error);
+    }
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,71 @@
+<template>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" placeholder="Search for a city or state" v-model="searchQuery" @input="getSearchResults"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
+      <ul class="absoute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <p v-if="searchError">
+          Sorry, something went wrong, please try again!
+        </p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term!
+        </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+          @click="previewCity(searchResult)">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  console.log(city+" "+state);
+  router.push({
+    name: "cityView",
+    params: {state: state.replaceAll(" ",""), city: city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
+
+const mapboxApiKey = "pk.eyJ1Ijoic2hvYWliLXJlaG1hbiIsImEiOiJjbHh1Y3dtYWsxemJnMnJxcmkza242MnF2In0.W190vY3NZeUlqzM4-8ngHg";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxApiKey}&types=place`);
+        mapboxSearchResults.value = result.data.features;
+      } catch (error) {
+        searchError.value = true; 
+      }
+      
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+
+</script>
+
+<style lang="scss" scoped></style>
